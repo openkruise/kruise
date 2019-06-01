beb0a71f03e619d58011a3e23a801a8c798f6293
He, Jian <jianhe788@gmail.com>
2019-05-31 23:46:09 -0700

    update script

--- hack/docker_build_daily.sh
+++ hack/docker_build_daily.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo $TRAVIS_EVENT_TYPE
-echo $TRAVIS_BRANCH
-echo "building container"
+echo $TRAVIS_EVENT_TYPE, $TRAVIS_BRANCH
 
 if [[ "$TRAVIS_EVENT_TYPE" = "cron" && "$TRAVIS_BRANCH" = "master" ]]; then
+    echo "building container"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
     make docker-build;
     make docker-push;
