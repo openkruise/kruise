c8286f11517e021c2d95a9a41adf9f2a99dd6745
He, Jian <jianhe788@gmail.com>
2019-07-29 22:34:08 -0700

    format

--- pkg/controller/broadcastjob/broadcastjob_controller.go
+++ pkg/controller/broadcastjob/broadcastjob_controller.go
@@ -193,7 +193,7 @@ func (r *ReconcileBroadcastJob) Reconcile(request reconcile.Request) (reconcile.
 	desiredNodes, restNodesToRunPod, podsToDelete := getNodesToRunPod(nodes, job, existingNodeToPodMap)
 	desired = int32(len(desiredNodes))
 	klog.Infof("%s/%s has %d/%d nodes remaining to schedule pods", job.Namespace, job.Name, len(restNodesToRunPod), desired)
-	klog.Infof("Before broadcastjob %s/%s, active=%d, failed=%d, desired=%d", job.Namespace, job.Name, active, failed, desired)
+	klog.Infof("Before broadcastjob %s/%s, desired=%d, active=%d, failed=%d", job.Namespace, job.Name, desired, active, failed)
 
 	jobFailed, failureReason, failureMessage := isJobFailed(job, pods)
 	// Job is failed. For keepAlive type, the job will never fail.
@@ -231,7 +231,7 @@ func (r *ReconcileBroadcastJob) Reconcile(request reconcile.Request) (reconcile.
 				fmt.Sprintf("Job %s/%s is completed, %d pods succeeded, %d pods failed", job.Namespace, job.Name, succeeded, failed))
 		}
 	}
-	klog.Infof("After broadcastjob %s/%s, active=%d, failed=%d, desired=%d", job.Namespace, job.Name, active, failed, desired)
+	klog.Infof("After broadcastjob %s/%s, desired=%d, active=%d, failed=%d", job.Namespace, job.Name, desired, active, failed)
 
 	// no need to update the job if the status hasn't changed since last time
 	if job.Status.Active != active || job.Status.Succeeded != succeeded ||
@@ -451,7 +451,9 @@ func getNodesToRunPod(nodes *corev1.NodeList, job *appsv1alpha1.BroadcastJob,
 				continue
 			}
 			if !canFit {
-				podsToDelete = append(podsToDelete, pod)
+				if pod.DeletionTimestamp == nil {
+					podsToDelete = append(podsToDelete, pod)
+				}
 				continue
 			}
 			desiredNodes[node.Name] = pod
