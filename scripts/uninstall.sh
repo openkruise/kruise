ef446e311570fa027763fef45606bfbc472e4e43
Siyu Wang <FillZpp.pub@gmail.com>
2019-09-06 15:51:41 +0800

    add uninstall script
    Signed-off-by: Siyu Wang <FillZpp.pub@gmail.com>

---
+++ scripts/uninstall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# delete webhook configurations
+kubectl delete mutatingwebhookconfiguration kruise-mutating-webhook-configuration
+kubectl delete validatingwebhookconfiguration kruise-validating-webhook-configuration
+
+# delete kruise-manager and rbac rules
+kubectl delete ns kruise-system
+kubectl delete clusterrolebinding kruise-manager-rolebinding
+kubectl delete clusterrole kruise-manager-role
+
+# delete CRDs
+kubectl get crd -o name | grep "customresourcedefinition.apiextensions.k8s.io/[a-z.]*.kruise.io" | xargs kubectl delete
