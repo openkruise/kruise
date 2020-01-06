505bd287b1ff89ddf457ff1745deb678ca7e9a8b
liuyan <liuyan@youzan.com>
2020-01-06 23:25:09 +0800

    feat: reduce AddToScheme import path

--- pkg/apis/addtoscheme_apps_v1alpha1.go
+++ pkg/apis/addtoscheme_apps_v1alpha1.go
@@ -22,5 +22,5 @@ import (
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v1alpha1.AddToScheme)
 }
