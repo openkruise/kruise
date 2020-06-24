fac2cdff233473e9c917d2d9cae8d84c67be811e
Allen <njuallen@foxmail.com>
2020-06-24 05:23:07 +0000

    Modified api of Pod Unavailable Budget. Added a lot of annotations.

--- pkg/apis/apps/v1alpha1/podunavailablebudget_types.go
+++ pkg/apis/apps/v1alpha1/podunavailablebudget_types.go
@@ -26,9 +26,12 @@ type PodUnavailableBudgetSpec struct {
 	// selector is a label query over pods that should be protected
 	Selector *metav1.LabelSelector `json:"selector,omitempty"`
 
-	// maximum unavailable pods
+	// number or percentage of maximum unavailable pods
+	// +optional
 	MaxUnavailable *intstr.IntOrString `json:"maxUnavailable,omitempty"`
 
+	// number or percentage of minimum available pods
+	// +optional
 	MinAvailable *intstr.IntOrString `json:"minAvailable,omitempty"`
 
 	// key-value pairs that indicates that pod is unavailable
@@ -38,32 +41,54 @@ type PodUnavailableBudgetSpec struct {
 	// But pod status won't change, since pod image does not change.
 	// To avoid any violation of MaxUnavailable,
 	// app-level update should add an unavailable label to their pod label before doing any update.
+	// +optional
 	UnavailableLabels map[string]string `json:unavailableLabels",omitempty"`
 }
 
 // PodUnavailableBudgetStatus defines the observed state of PodUnavailableBudgetStatus
 type PodUnavailableBudgetStatus struct {
-	// observedGeneration is the most recent generation observed for this SidecarSet. It corresponds to the
-	// SidecarSet's generation, which is updated on mutation by the API Server.
+	// observedGeneration is the most recent generation observed for this PodUnavailableBudget. It corresponds to the
+	// PodUnavailableBudget's generation, which is updated on mutation by the API Server.
 	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 
+	// DeletedPods contains **uids** of pods that are deleted or to be deleted.
+	// When pod delete request is audited and approved by PodUnavailableBudget's validating webhook,
+	// the pod's uid will be added to this list.
+	// Since then, this pod will be treated as both unavailable and deleted by PodUnavailableBudget's controller,
+	// even if it may appear to be available for a short while.
+	// Note that:
+	//  * pods in this list are deleted pods
+	//  * not all deleted pods end up in this list.
+	//    Those pods that are deleted before PUB starts will not show up here.
 	DeletedPods []string `json:"deletedPods,omitempty"`
+
+	// UnavailablePods contains **names** of pods that are unavailable.
+	// When a pod becomes unavailable or will be kicked unavailable in a short while,
+	// PodUnavailableBudget's validating webhook will add its name to this list.
+	// Since then, this pod will be treated as unavailable by PodUnavailableBudget's controller,
+	// even if it may appear to be available for a short while.
 	UnavailablePods []string `json:"unavailablePods,omitempty"`
 
+	// pod unavailablility allowed (the number of pods that can be kicked unavailable) at this moment.
 	UnavailableAllowed int32 `json:"unavailableAllowed"`
 
 	// number of available pods at this moment
 	CurrentAvailable int32 `json:"currentAvailable"`
 
+	// number of unavailable pods at this moment
 	CurrentUnavailable int32 `json:"currentUnavailable"`
 
-	TotalReplicas int32 `json:"totalReplicas"`
+	// total number of pods at this moment
+	CurrentPodCount int32 `json:"currentPodCount"`
+
+	// expected number of replicas in total as specified by pods' controllers
+	ExpectedTotalReplicas int32 `json:"expectedTotalReplicas"`
 }
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// SidecarSet is the Schema for the sidecarsets API
+// PodUnavailableBudget is the Schema for the PodUnavailableBudget API
 // +k8s:openapi-gen=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="MATCHED",type="integer",JSONPath=".status.matchedPods",description="The number of pods matched."
