038cb120e2e87f5fed7c82e5b8e266d66a02bb58
Rishi <rishi.anand0@gmail.com>
2020-12-02 09:16:12 +0530

    Generating manifests to include api changes in crd

--- pkg/webhook/advancedcronjob/validating/webhooks.go
+++ pkg/webhook/advancedcronjob/validating/webhooks.go
@@ -17,7 +17,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-// +kubebuilder:webhook:path=/validate-apps-kruise-io-v1alpha1-advancedcronjob,mutating=false,failurePolicy=fail,groups=apps.kruise.io,resources=advancedcronjobs,verbs=create;update,versions=v1alpha1,name=madvancedcronjob.kb.io
+// +kubebuilder:webhook:path=/validate-apps-kruise-io-v1alpha1-advancedcronjob,mutating=false,failurePolicy=fail,groups=apps.kruise.io,resources=advancedcronjobs,verbs=create;update,versions=v1alpha1,name=vadvancedcronjob.kb.io
 
 var (
 	// HandlerMap contains admission webhook handlers
