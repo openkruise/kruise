6a4c7e517dcf70696ff53cba9de241a4b3b36e8d
FillZpp <FillZpp.pub@gmail.com>
2021-03-16 11:17:13 +0800

    Update new channel address and scripts
    Signed-off-by: FillZpp <FillZpp.pub@gmail.com>

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,8 +6,8 @@ kubectl delete validatingwebhookconfiguration kruise-validating-webhook-configur
 
 # delete kruise-manager and rbac rules
 kubectl delete ns kruise-system
-kubectl delete clusterrolebinding kruise-manager-rolebinding
-kubectl delete clusterrole kruise-manager-role
+kubectl delete clusterrolebinding kruise-manager-rolebinding kruise-daemon-rolebinding
+kubectl delete clusterrole kruise-manager-role kruise-daemon-role
 
 # delete CRDs
 kubectl get crd -o name | grep "customresourcedefinition.apiextensions.k8s.io/[a-z.]*.kruise.io" | xargs kubectl patch -p '{"spec":{"conversion":null}}'
