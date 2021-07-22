7d43ba9c065b6cce88be0836869a4e46a4d7f580
berg <liheng.zms@alibaba-inc.com>
2021-07-22 16:18:56 +0800

    podUnavailableBudget for pod validate webhook (#666)
    Signed-off-by: liheng.zms <liheng.zms@alibaba-inc.com>

---
+++ pkg/webhook/pod/validating/pod_create_update_handler.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2021 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validating
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/openkruise/kruise/pkg/util/controllerfinder"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// PodCreateHandler handles Pod
+type PodCreateHandler struct {
+	// To use the client, you need to do the following:
+	// - uncomment it
+	// - import sigs.k8s.io/controller-runtime/pkg/client
+	// - uncomment the InjectClient method at the bottom of this file.
+	Client client.Client
+
+	// Decoder decodes objects
+	Decoder *admission.Decoder
+
+	controllerFinder *controllerfinder.ControllerFinder
+}
+
+func (h *PodCreateHandler) validatingPodFn(ctx context.Context, req admission.Request) (allowed bool, reason string, err error) {
+	return h.podUnavailableBudgetValidatingPod(ctx, req)
+}
+
+var _ admission.Handler = &PodCreateHandler{}
+
+// Handle handles admission requests.
+func (h *PodCreateHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
+	allowed, reason, err := h.validatingPodFn(ctx, req)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	return admission.ValidationResponse(allowed, reason)
+}
+
+var _ inject.Client = &PodCreateHandler{}
+
+// InjectClient injects the client into the PodCreateHandler
+func (h *PodCreateHandler) InjectClient(c client.Client) error {
+	h.Client = c
+	h.controllerFinder = controllerfinder.NewControllerFinder(c)
+	return nil
+}
+
+var _ admission.DecoderInjector = &PodCreateHandler{}
+
+// InjectDecoder injects the decoder into the PodCreateHandler
+func (h *PodCreateHandler) InjectDecoder(d *admission.Decoder) error {
+	h.Decoder = d
+	return nil
+}
