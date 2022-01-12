ee53646aa62cf83e321c798960a930167b30c8bf
shiyan2016 <shiyan20160606@gmail.com>
2022-01-12 15:23:24 +0800

    break the loop when we find the current revision
    Signed-off-by: shiyan2016 <shiyan20160606@gmail.com>

--- pkg/controller/statefulset/stateful_set_control.go
+++ pkg/controller/statefulset/stateful_set_control.go
@@ -263,6 +263,7 @@ func (ssc *defaultStatefulSetControl) getStatefulSetRevisions(
 	for i := range revisions {
 		if revisions[i].Name == set.Status.CurrentRevision {
 			currentRevision = revisions[i]
+			break
 		}
 	}
 
