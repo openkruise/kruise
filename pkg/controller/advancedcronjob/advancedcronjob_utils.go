b3719fe6250e6063b8e90ff4a4abef8b77add81d
FillZpp <FillZpp.pub@gmail.com>
2022-09-06 14:14:14 +0800

    Support timezone for AdvancedCronJob
    Signed-off-by: FillZpp <FillZpp.pub@gmail.com>

--- pkg/controller/advancedcronjob/advancedcronjob_utils.go
+++ pkg/controller/advancedcronjob/advancedcronjob_utils.go
@@ -1,6 +1,13 @@
 package advancedcronjob
 
-import appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	"k8s.io/klog/v2"
+)
 
 func FindTemplateKind(spec appsv1alpha1.AdvancedCronJobSpec) appsv1alpha1.TemplateKind {
 	if spec.Template.JobTemplate != nil {
@@ -8,4 +15,18 @@ func FindTemplateKind(spec appsv1alpha1.AdvancedCronJobSpec) appsv1alpha1.Templa
 	}
 
 	return appsv1alpha1.BroadcastJobTemplate
+}
+
+func formatSchedule(acj *appsv1alpha1.AdvancedCronJob) string {
+	if strings.Contains(acj.Spec.Schedule, "TZ") {
+		return acj.Spec.Schedule
+	}
+	if acj.Spec.TimeZone != nil {
+		if _, err := time.LoadLocation(*acj.Spec.TimeZone); err != nil {
+			klog.Errorf("Failed to load location %s for %s/%s: %v", *acj.Spec.TimeZone, acj.Namespace, acj.Name, err)
+			return acj.Spec.Schedule
+		}
+		return fmt.Sprintf("TZ=%s %s", *acj.Spec.TimeZone, acj.Spec.Schedule)
+	}
+	return acj.Spec.Schedule
 }
