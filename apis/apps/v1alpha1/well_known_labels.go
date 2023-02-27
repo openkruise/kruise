c82784bdb0f3b4bcf6d76d0be8fb41a2e69d52dc
Wei-Xiang Sun <Vec.G.Sun@gmail.com>
2023-02-27 10:48:14 +0800

    add sidecar terminator controller (#1051)
    Signed-off-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>
    Co-authored-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>

--- apis/apps/v1alpha1/well_known_labels.go
+++ apis/apps/v1alpha1/well_known_labels.go
@@ -16,3 +16,15 @@ const (
 	// ImagePreDownloadIgnoredKey indicates the images of this revision have been ignored to pre-download
 	ImagePreDownloadIgnoredKey = "apps.kruise.io/image-predownload-ignored"
 )
+
+// Sidecar container environment variable definitions which are used to enable SidecarTerminator to take effect on the sidecar container.
+const (
+	// KruiseTerminateSidecarEnv is an env name, which represents a switch to enable sidecar terminator.
+	// The corresponding value is "true", which means apply a crr to kill sidecar using kruise-daemon.
+	KruiseTerminateSidecarEnv = "KRUISE_TERMINATE_SIDECAR_WHEN_JOB_EXIT"
+
+	// KruiseTerminateSidecarWithImageEnv is an env name, which refers to an image that will replace the original image
+	// using in-place update strategy to kill sidecar. This image must be given if you want to use in-place update
+	// strategy to terminate sidecar containers.
+	KruiseTerminateSidecarWithImageEnv = "KRUISE_TERMINATE_SIDECAR_WHEN_JOB_EXIT_WITH_IMAGE"
+)
