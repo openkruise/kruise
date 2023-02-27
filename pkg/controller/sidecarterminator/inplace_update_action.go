597eac935f68d70fb555325f29965f65915d8baa
Wei-Xiang Sun <Vec.G.Sun@gmail.com>
2023-02-27 10:48:14 +0800

    add sidecar terminator controller (#1051)
    Signed-off-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>
    Co-authored-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>
    Signed-off-by: 李龙峰 <lilongfeng@inspur.com>

---
+++ pkg/controller/sidecarterminator/inplace_update_action.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2023 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sidecarterminator
+
+import (
+	"context"
+
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/util/retry"
+	"k8s.io/klog/v2"
+)
+
+func (r *ReconcileSidecarTerminator) executeInPlaceUpdateAction(originalPod *corev1.Pod, sidecars sets.String) error {
+	uncompletedSidecars := filterUncompletedSidecars(originalPod, sidecars)
+	if uncompletedSidecars.Len() == 0 {
+		return nil
+	}
+
+	var changed bool
+	var pod *corev1.Pod
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		pod = &corev1.Pod{}
+		getErr := r.Client.Get(context.TODO(), types.NamespacedName{Name: originalPod.Name, Namespace: originalPod.Namespace}, pod)
+		if getErr != nil {
+			if errors.IsNotFound(getErr) {
+				return nil
+			}
+			return getErr
+		}
+
+		changed, pod = updateSidecarsForInPlaceUpdate(pod, uncompletedSidecars)
+		if !changed || pod == nil {
+			return nil
+		}
+
+		return r.Update(context.TODO(), pod)
+	})
+
+	if err != nil {
+		klog.Errorf("SidecarTerminator -- Error occurred when inPlace update pod(%v/%v), error %v",
+			originalPod.Namespace, originalPod.Name, err)
+	} else {
+		klog.V(3).Infof("SidecarTerminator -- InPlace update pod(%v/%v) successfully",
+			originalPod.Namespace, originalPod.Name)
+
+		r.recorder.Eventf(originalPod, corev1.EventTypeNormal, "SidecarTerminator",
+			"Kruise SidecarTerminator is trying to terminate sidecar %v using in-place update", uncompletedSidecars.List())
+	}
+
+	return err
+}
+
+// updateSidecarsForInPlaceUpdate replace sidecar image with KruiseTerminateSidecarWithImageEnv
+func updateSidecarsForInPlaceUpdate(pod *corev1.Pod, sidecars sets.String) (bool, *corev1.Pod) {
+	changed := false
+	for i := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[i]
+		if !sidecars.Has(container.Name) {
+			continue
+		}
+		changed = true
+		container.Image = getImageFromEnv(container)
+	}
+	return changed, pod
+}
+
+// getImageFromEnv return the image set in sidecar env
+func getImageFromEnv(container *corev1.Container) string {
+	for i := range container.Env {
+		if container.Env[i].Name == appsv1alpha1.KruiseTerminateSidecarWithImageEnv {
+			return container.Env[i].Value
+		}
+	}
+	return ""
+}
---
+++ test/e2e/apps/sidecarterminator.go
@@ -0,0 +1,430 @@
+package apps
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	kruiseclientset "github.com/openkruise/kruise/pkg/client/clientset/versioned"
+	"github.com/openkruise/kruise/test/e2e/framework"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/utils/pointer"
+)
+
+var _ = SIGDescribe("SidecarTerminator", func() {
+	f := framework.NewDefaultFramework("sidecarterminator")
+	var ns string
+	var c clientset.Interface
+	var kc kruiseclientset.Interface
+	var randStr string
+
+	ginkgo.BeforeEach(func() {
+		c = f.ClientSet
+		kc = f.KruiseClientSet
+		ns = f.Namespace.Name
+		randStr = rand.String(10)
+	})
+
+	framework.KruiseDescribe("SidecarTerminator checker", func() {
+		ginkgo.It("use kruise-daemon to kill containers", func() {
+			mainContainer := v1.Container{
+				Name:            "main",
+				Image:           "busybox:latest",
+				ImagePullPolicy: v1.PullIfNotPresent,
+				Command:         []string{"/bin/sh", "-c", "sleep 5"},
+			}
+			sidecarContainer := v1.Container{
+				Name:            "sidecar",
+				Image:           "nginx:latest",
+				ImagePullPolicy: v1.PullIfNotPresent,
+				Env: []v1.EnvVar{
+					{
+						Name:  appsv1alpha1.KruiseTerminateSidecarEnv,
+						Value: "true",
+					},
+				},
+			}
+
+			cases := []struct {
+				name        string
+				createJob   func(str string) metav1.Object
+				checkStatus func(object metav1.Object) bool
+			}{
+				{
+					name: "native job, restartPolicy=Never",
+					createJob: func(str string) metav1.Object {
+						job := &batchv1.Job{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: batchv1.JobSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											*mainContainer.DeepCopy(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyNever,
+									},
+								},
+							},
+						}
+						job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := c.BatchV1().Jobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						for _, cond := range job.Status.Conditions {
+							if cond.Type == batchv1.JobComplete {
+								return true
+							}
+						}
+						return false
+					},
+				},
+				{
+					name: "native job, restartPolicy=OnFailure, main failed",
+					createJob: func(str string) metav1.Object {
+						job := &batchv1.Job{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: batchv1.JobSpec{
+								BackoffLimit: pointer.Int32Ptr(3),
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											func() v1.Container {
+												main := mainContainer.DeepCopy()
+												main.Command = []string{"/bin/sh", "-c", "exit 1"}
+												return *main
+											}(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyOnFailure,
+									},
+								},
+							},
+						}
+						job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := c.BatchV1().Jobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						for _, cond := range job.Status.Conditions {
+							if cond.Type == batchv1.JobFailed {
+								return true
+							}
+						}
+						return false
+					},
+				},
+				{
+					name: "native job, restartPolicy=OnFailure, main succeeded",
+					createJob: func(str string) metav1.Object {
+						job := &batchv1.Job{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: batchv1.JobSpec{
+								BackoffLimit: pointer.Int32Ptr(3),
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											*mainContainer.DeepCopy(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyOnFailure,
+									},
+								},
+							},
+						}
+						job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := c.BatchV1().Jobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						for _, cond := range job.Status.Conditions {
+							if cond.Type == batchv1.JobComplete {
+								return true
+							}
+						}
+						return false
+					},
+				},
+				{
+					name: "BroadcastJob, restartPolicy=Never",
+					createJob: func(str string) metav1.Object {
+						job := &appsv1alpha1.BroadcastJob{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: appsv1alpha1.BroadcastJobSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											*mainContainer.DeepCopy(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyNever,
+									},
+								},
+								CompletionPolicy: appsv1alpha1.CompletionPolicy{Type: appsv1alpha1.Always},
+							},
+						}
+						job, err := kc.AppsV1alpha1().BroadcastJobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := kc.AppsV1alpha1().BroadcastJobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job.Status.Phase == appsv1alpha1.PhaseCompleted
+					},
+				},
+				{
+					name: "BroadcastJob, restartPolicy=OnFailure, main failed",
+					createJob: func(str string) metav1.Object {
+						job := &appsv1alpha1.BroadcastJob{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: appsv1alpha1.BroadcastJobSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											func() v1.Container {
+												main := mainContainer.DeepCopy()
+												main.Command = []string{"/bin/sh", "-c", "exit 1"}
+												return *main
+											}(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyOnFailure,
+									},
+								},
+								CompletionPolicy: appsv1alpha1.CompletionPolicy{Type: appsv1alpha1.Always},
+							},
+						}
+						job, err := kc.AppsV1alpha1().BroadcastJobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := kc.AppsV1alpha1().BroadcastJobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job.Status.Phase == appsv1alpha1.PhaseFailed
+					},
+				},
+				{
+					name: "BroadcastJob, restartPolicy=OnFailure, main succeeded",
+					createJob: func(str string) metav1.Object {
+						job := &appsv1alpha1.BroadcastJob{
+							ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+							Spec: appsv1alpha1.BroadcastJobSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Containers: []v1.Container{
+											*mainContainer.DeepCopy(),
+											*sidecarContainer.DeepCopy(),
+										},
+										RestartPolicy: v1.RestartPolicyOnFailure,
+									},
+								},
+								CompletionPolicy: appsv1alpha1.CompletionPolicy{Type: appsv1alpha1.Always},
+							},
+						}
+						job, err := kc.AppsV1alpha1().BroadcastJobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job
+					},
+					checkStatus: func(object metav1.Object) bool {
+						job, err := kc.AppsV1alpha1().BroadcastJobs(object.GetNamespace()).
+							Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+						gomega.Expect(err).NotTo(gomega.HaveOccurred())
+						return job.Status.Phase == appsv1alpha1.PhaseCompleted
+					},
+				},
+			}
+
+			for i := range cases {
+				ginkgo.By(cases[i].name)
+				ginkgo.By("Create job-" + randStr)
+				job := cases[i].createJob(fmt.Sprintf("%v-%v", randStr, i))
+				time.Sleep(time.Second)
+				gomega.Eventually(func() bool {
+					return cases[i].checkStatus(job)
+				}, 10*time.Minute, time.Second).Should(gomega.BeTrue())
+			}
+		})
+		/*
+			ginkgo.It("use in-place update strategy to kill containers", func() {
+				mainContainer := v1.Container{
+					Name:            "main",
+					Image:           "busybox:latest",
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Command:         []string{"/bin/sh", "-c", "sleep 5"},
+				}
+				sidecarContainer := v1.Container{
+					Name:            "sidecar",
+					Image:           "nginx:latest",
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Env: []v1.EnvVar{
+						{
+							Name:  stutil.KruiseTerminateSidecarEnv,
+							Value: stutil.InplaceUpdateONLYStrategy,
+						},
+						{
+							Name:  stutil.KruiseTerminateSidecarWithImageEnv,
+							Value: "minchou/exit:v1.0",
+						},
+						{
+							Name:  stutil.KruiseTerminateSidecarWithDownwardAPI,
+							Value: "true",
+						},
+					},
+				}
+
+				cases := []struct {
+					name        string
+					createJob   func(str string) metav1.Object
+					checkStatus func(object metav1.Object) bool
+				}{
+					{
+						name: "native job, no command",
+						createJob: func(str string) metav1.Object {
+							job := &batchv1.Job{
+								ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+								Spec: batchv1.JobSpec{
+									Template: v1.PodTemplateSpec{
+										Spec: v1.PodSpec{
+											Containers: []v1.Container{
+												*mainContainer.DeepCopy(),
+												*sidecarContainer.DeepCopy(),
+											},
+											RestartPolicy: v1.RestartPolicyNever,
+										},
+									},
+								},
+							}
+							fmt.Println("Batch Job")
+							job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							return job
+						},
+						checkStatus: func(object metav1.Object) bool {
+							job, err := c.BatchV1().Jobs(object.GetNamespace()).
+								Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							for _, cond := range job.Status.Conditions {
+								if cond.Type == batchv1.JobComplete {
+									return true
+								}
+							}
+							return false
+						},
+					},
+					{
+						name: "native job, 3 commands",
+						createJob: func(str string) metav1.Object {
+							job := &batchv1.Job{
+								ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+								Spec: batchv1.JobSpec{
+									BackoffLimit: pointer.Int32Ptr(3),
+									Template: v1.PodTemplateSpec{
+										Spec: v1.PodSpec{
+											Containers: []v1.Container{
+												func() v1.Container {
+													main := mainContainer.DeepCopy()
+													main.Command = []string{"/bin/sh", "-c", "exit 1"}
+													return *main
+												}(),
+												func() v1.Container {
+													sidecar := sidecarContainer.DeepCopy()
+													sidecar.Command = []string{"/bin/sh", "-c", "sleep 10000"}
+													return *sidecar
+												}(),
+											},
+											RestartPolicy: v1.RestartPolicyOnFailure,
+										},
+									},
+								},
+							}
+							job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							return job
+						},
+						checkStatus: func(object metav1.Object) bool {
+							job, err := c.BatchV1().Jobs(object.GetNamespace()).
+								Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							for _, cond := range job.Status.Conditions {
+								if cond.Type == batchv1.JobFailed {
+									return true
+								}
+							}
+							return false
+						},
+					},
+					{
+						name: "native job, 4 commands",
+						createJob: func(str string) metav1.Object {
+							job := &batchv1.Job{
+								ObjectMeta: metav1.ObjectMeta{Namespace: ns, Name: "job-" + str},
+								Spec: batchv1.JobSpec{
+									BackoffLimit: pointer.Int32Ptr(3),
+									Template: v1.PodTemplateSpec{
+										Spec: v1.PodSpec{
+											Containers: []v1.Container{
+												*mainContainer.DeepCopy(),
+												func() v1.Container {
+													sidecar := sidecarContainer.DeepCopy()
+													sidecar.Command = []string{"/bin/sh", "-c", "sleep 10000"}
+													sidecar.Args = []string{`echo "hello"`}
+													return *sidecar
+												}(),
+											},
+											RestartPolicy: v1.RestartPolicyOnFailure,
+										},
+									},
+								},
+							}
+							job, err := c.BatchV1().Jobs(ns).Create(context.TODO(), job, metav1.CreateOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							return job
+						},
+						checkStatus: func(object metav1.Object) bool {
+							job, err := c.BatchV1().Jobs(object.GetNamespace()).
+								Get(context.TODO(), object.GetName(), metav1.GetOptions{})
+							gomega.Expect(err).NotTo(gomega.HaveOccurred())
+							for _, cond := range job.Status.Conditions {
+								if cond.Type == batchv1.JobComplete {
+									return true
+								}
+							}
+							return false
+						},
+					},
+				}
+
+				for i := range cases {
+					ginkgo.By(cases[i].name)
+					ginkgo.By("Create job-" + randStr)
+					job := cases[i].createJob(fmt.Sprintf("%v-%v", randStr, i))
+					time.Sleep(time.Second)
+					gomega.Eventually(func() bool {
+						return cases[i].checkStatus(job)
+					}, 5*time.Minute, time.Second).Should(gomega.BeTrue())
+				}
+			})
+		*/
+	})
+})
