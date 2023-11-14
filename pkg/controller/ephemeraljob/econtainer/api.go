01717ff47d1247f4b1ccc1caf74272f64daf33a2
Wei-Xiang Sun <Vec.G.Sun@gmail.com>
2023-11-14 11:05:08 +0800

    add some important events for ephemeral job (#1454)
    Signed-off-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>
    Co-authored-by: mingzhou.swx <mingzhou.swx@alibaba-inc.com>

--- pkg/controller/ephemeraljob/econtainer/api.go
+++ pkg/controller/ephemeraljob/econtainer/api.go
@@ -1,21 +1,28 @@
 package econtainer
 
 import (
+	"time"
+
 	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
 	v1 "k8s.io/api/core/v1"
 )
 
 type EphemeralContainerInterface interface {
-	// GetEphemeralContainers will return all ephemeral container status.
+	// GetEphemeralContainersStatus will return all ephemeral container status.
 	// Maybe they are not created by current ephemeral jobs.
 	GetEphemeralContainersStatus(target *v1.Pod) []v1.ContainerStatus
 	// GetEphemeralContainers return all ephemeral containers which have been created in target pods.
 	// Maybe they are not created by current ephemeral jobs.
 	GetEphemeralContainers(target *v1.Pod) []v1.EphemeralContainer
+	// ContainsEphemeralContainer return if target pod contains(1st return value) and owns(2nd return value)
+	// the ephemeral containers having the same name with the ones ephemeralJob want to inject.
+	// Owning an ephemeral containers to a ephemeralJob means KRUISE_EJOB_ID env of the ephemeral container
+	// equals to this ephemeralJob's uid.
+	ContainsEphemeralContainer(target *v1.Pod) (bool, bool)
 
 	UpdateEphemeralContainer(target *v1.Pod) error
 	CreateEphemeralContainer(target *v1.Pod) error
-	RemoveEphemeralContainer(target *v1.Pod) error
+	RemoveEphemeralContainer(target *v1.Pod) (*time.Duration, error)
 }
 
 func New(job *appsv1alpha1.EphemeralJob) EphemeralContainerInterface {
