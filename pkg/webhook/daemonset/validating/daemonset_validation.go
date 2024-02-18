0af51ff594f3c999040f7457a710c22a6f0f2d6c
hantmac <hantmac@outlook.com>
2024-02-18 23:17:29 +0800

    fix: validate update AdvancedDaemonSet
    Signed-off-by: hantmac <hantmac@outlook.com>
    
    refactor it
    
    fix
    
    fix
    
    Signed-off-by: hantmac <hantmac@outlook.com>

--- pkg/webhook/daemonset/validating/daemonset_validation.go
+++ pkg/webhook/daemonset/validating/daemonset_validation.go
@@ -1,30 +1,10 @@
-/*
-Copyright 2020 The Kruise Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package validating
 
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"strconv"
 
-	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
-	webhookutil "github.com/openkruise/kruise/pkg/webhook/util"
-	"github.com/openkruise/kruise/pkg/webhook/util/convertor"
 	corev1 "k8s.io/api/core/v1"
 	genericvalidation "k8s.io/apimachinery/pkg/api/validation"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,24 +13,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	"k8s.io/klog/v2"
 	appsvalidation "k8s.io/kubernetes/pkg/apis/apps/validation"
 	corevalidation "k8s.io/kubernetes/pkg/apis/core/validation"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	appsv1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
+	webhookutil "github.com/openkruise/kruise/pkg/webhook/util"
+	"github.com/openkruise/kruise/pkg/webhook/util/convertor"
 )
 
-// ValidateDaemonSetName can be used to check whether the given daemon set name is valid.
-// Prefix indicates this name will be used as part of generation, in which case
-// trailing dashes are allowed.
-var ValidateDaemonSetName = genericvalidation.NameIsDNSSubdomain
-
-// DaemonSetCreateUpdateHandler handles DaemonSet
-type DaemonSetCreateUpdateHandler struct {
-	// Decoder decodes objects
-	Decoder *admission.Decoder
-}
-
-func (h *DaemonSetCreateUpdateHandler) validatingDaemonSetFn(ctx context.Context, obj *appsv1alpha1.DaemonSet) (bool, string, error) {
+func validatingDaemonSetFn(ctx context.Context, obj *appsv1alpha1.DaemonSet) (bool, string, error) {
 	allErrs := validateDaemonSet(obj)
 	if len(allErrs) != 0 {
 		return false, "", allErrs.ToAggregate()
@@ -194,30 +165,4 @@ func getPercentValue(intOrStringValue intstr.IntOrString) (int, bool) {
 	}
 	value, _ := strconv.Atoi(intOrStringValue.StrVal[:len(intOrStringValue.StrVal)-1])
 	return value, true
-}
-
-var _ admission.Handler = &DaemonSetCreateUpdateHandler{}
-
-// Handle handles admission requests.
-func (h *DaemonSetCreateUpdateHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
-	obj := &appsv1alpha1.DaemonSet{}
-
-	err := h.Decoder.Decode(req, obj)
-	if err != nil {
-		return admission.Errored(http.StatusBadRequest, err)
-	}
-	allowed, reason, err := h.validatingDaemonSetFn(ctx, obj)
-	if err != nil {
-		klog.Warningf("ds %s/%s action %v fail:%s", obj.Namespace, obj.Name, req.AdmissionRequest.Operation, err.Error())
-		return admission.Errored(http.StatusInternalServerError, err)
-	}
-	return admission.ValidationResponse(allowed, reason)
-}
-
-var _ admission.DecoderInjector = &DaemonSetCreateUpdateHandler{}
-
-// InjectDecoder injects the decoder into the DaemonSetCreateUpdateHandler
-func (h *DaemonSetCreateUpdateHandler) InjectDecoder(d *admission.Decoder) error {
-	h.Decoder = d
-	return nil
 }
