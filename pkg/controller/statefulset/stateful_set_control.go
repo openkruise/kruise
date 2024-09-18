1dd887136422e7d9b6e8eaa04e0163351813d95f
Abner <yuanyuxing.yyx@alibaba-inc.com>
2024-09-18 15:34:14 +0800

    support specified-delete in asts (#1734)
    Signed-off-by: Abner-1 <yuanyuxing.yyx@alibaba-inc.com>

--- pkg/controller/statefulset/stateful_set_control.go
+++ pkg/controller/statefulset/stateful_set_control.go
@@ -28,6 +28,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	intstrutil "k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/controller/history"
@@ -42,6 +43,7 @@ import (
 	imagejobutilfunc "github.com/openkruise/kruise/pkg/util/imagejob/utilfunction"
 	"github.com/openkruise/kruise/pkg/util/inplaceupdate"
 	"github.com/openkruise/kruise/pkg/util/lifecycle"
+	"github.com/openkruise/kruise/pkg/util/specifieddelete"
 )
 
 // StatefulSetControlInterface implements the control logic for updating StatefulSets and their children Pods. It is implemented
@@ -499,7 +501,7 @@ func (ssc *defaultStatefulSetControl) updateStatefulSet(
 				set.Namespace,
 				set.Name,
 				replicas[i].Name)
-			if _, err := ssc.deletePod(set, replicas[i]); err != nil {
+			if _, _, err := ssc.deletePod(set, replicas[i]); err != nil {
 				return &status, err
 			}
 			if getPodRevision(replicas[i]) == currentRevision.Name {
@@ -684,7 +686,7 @@ func (ssc *defaultStatefulSetControl) updateStatefulSet(
 			set.Name,
 			condemned[target].Name)
 
-		modified, err := ssc.deletePod(set, condemned[target])
+		modified, _, err := ssc.deletePod(set, condemned[target])
 		if err != nil || modified {
 			return &status, err
 		}
@@ -796,11 +798,17 @@ func (ssc *defaultStatefulSetControl) rollingUpdateStatefulsetPods(
 		}
 	}
 
+	// handle specified deleted pod under maxUnavailable constrain
+	// NOTE: specified deletion is not constraint by partition setting
+	specifiedDeletedPods, err := ssc.handleSpecifiedDeletedPods(set, status, currentRevision, updateRevision, replicas, maxUnavailable, unavailablePods)
+	if err != nil {
+		return status, err
+	}
+
 	updateIndexes := sortPodsToUpdate(set.Spec.UpdateStrategy.RollingUpdate, updateRevision.Name, *set.Spec.Replicas, replicas)
 	klog.V(3).Infof("Prepare to update pods indexes %v for StatefulSet %s", updateIndexes, getStatefulSetKey(set))
 	// update pods in sequence
 	for _, target := range updateIndexes {
-
 		// the target is already up-to-date, go to next
 		if getPodRevision(replicas[target]) == updateRevision.Name {
 			continue
@@ -819,25 +827,26 @@ func (ssc *defaultStatefulSetControl) rollingUpdateStatefulsetPods(
 		}
 
 		// delete the Pod if it is not already terminating and does not match the update revision.
-		if !isTerminating(replicas[target]) {
+		if !specifiedDeletedPods.Has(replicas[target].Name) && !isTerminating(replicas[target]) {
 			// todo validate in-place for pub
 			inplacing, inplaceUpdateErr := ssc.inPlaceUpdatePod(set, replicas[target], updateRevision, revisions)
 			if inplaceUpdateErr != nil {
 				return status, inplaceUpdateErr
 			}
+			// if pod is inplacing or actual deleting, decrease revision
+			revisionNeedDecrease := inplacing
 			if !inplacing {
-				klog.V(2).Infof("StatefulSet %s/%s terminating Pod %s for update",
-					set.Namespace,
-					set.Name,
-					replicas[target].Name)
-				if _, err := ssc.deletePod(set, replicas[target]); err != nil {
+				klog.V(2).InfoS("StatefulSet terminating Pod for update", "statefulSet", klog.KObj(set), "pod", klog.KObj(replicas[target]))
+				if _, actualDeleting, err := ssc.deletePod(set, replicas[target]); err != nil {
 					return status, err
+				} else {
+					revisionNeedDecrease = actualDeleting
 				}
 			}
 			// mark target as unavailable because it's updated
 			unavailablePods.Insert(replicas[target].Name)
 
-			if getPodRevision(replicas[target]) == currentRevision.Name {
+			if revisionNeedDecrease && getPodRevision(replicas[target]) == currentRevision.Name {
 				status.CurrentReplicas--
 			}
 		}
@@ -846,23 +855,63 @@ func (ssc *defaultStatefulSetControl) rollingUpdateStatefulsetPods(
 	return status, nil
 }
 
-func (ssc *defaultStatefulSetControl) deletePod(set *appsv1beta1.StatefulSet, pod *v1.Pod) (bool, error) {
+func (ssc *defaultStatefulSetControl) handleSpecifiedDeletedPods(
+	set *appsv1beta1.StatefulSet,
+	status *appsv1beta1.StatefulSetStatus,
+	currentRevision *apps.ControllerRevision,
+	updateRevision *apps.ControllerRevision,
+	replicas []*v1.Pod,
+	maxUnavailable int,
+	unavailablePods sets.String) (sets.String, error) {
+	specifiedDeletedPods := sets.NewString()
+	for target := len(replicas) - 1; target >= 0; target-- {
+		if replicas[target] == nil || !specifieddelete.IsSpecifiedDelete(replicas[target]) {
+			continue
+		}
+		// the unavailable pods count exceed the maxUnavailable and the target is available, so we can't process it,
+		// why skip here rather than return?
+		// case: pod 0 ready, pod1 unready, pod 2 unready, pod3 ready, pod4 ready
+		// when maxUnavailable = 3, pod4 with specified deleted will be deleted but pod3 can't
+		// pod 2 and pod 1 can be deleted because they were unavailable
+		if len(unavailablePods) >= maxUnavailable && !unavailablePods.Has(replicas[target].Name) {
+			klog.V(4).InfoS("StatefulSet was waiting for unavailable Pods to update, blocked pod",
+				"statefulSet", klog.KObj(set), "unavailablePods", unavailablePods.List(), "blockedPod", klog.KObj(replicas[target]))
+			continue
+		}
+
+		specifiedDeletedPods.Insert(replicas[target].Name)
+		if _, actualDeleting, err := ssc.deletePod(set, replicas[target]); err != nil {
+			return specifiedDeletedPods, err
+		} else if actualDeleting {
+			// if actual deleted, update revision count in status
+			if getPodRevision(replicas[target]) == currentRevision.Name {
+				status.CurrentReplicas--
+			} else if getPodRevision(replicas[target]) == updateRevision.Name {
+				status.UpdatedReplicas--
+			}
+		}
+		// mark target as unavailable because it's deleting or pre-deleting
+		unavailablePods.Insert(replicas[target].Name)
+	}
+	return specifiedDeletedPods, nil
+}
+
+func (ssc *defaultStatefulSetControl) deletePod(set *appsv1beta1.StatefulSet, pod *v1.Pod) (modified, actualDeleting bool, err error) {
 	if set.Spec.Lifecycle != nil && lifecycle.IsPodHooked(set.Spec.Lifecycle.PreDelete, pod) {
 		markPodNotReady := set.Spec.Lifecycle.PreDelete.MarkPodNotReady
 		if updated, _, err := ssc.lifecycleControl.UpdatePodLifecycle(pod, appspub.LifecycleStatePreparingDelete, markPodNotReady); err != nil {
-			return false, err
+			return false, false, err
 		} else if updated {
-			klog.V(3).Infof("StatefulSet %s scaling update pod %s lifecycle to PreparingDelete",
-				getStatefulSetKey(set), pod.Name)
-			return true, nil
+			klog.V(3).InfoS("StatefulSet scaling update pod lifecycle to PreparingDelete", "statefulSet", klog.KObj(set), "pod", klog.KObj(pod))
+			return true, false, nil
 		}
-		return false, nil
+		return false, false, nil
 	}
 	if err := ssc.podControl.DeleteStatefulPod(set, pod); err != nil {
 		ssc.recorder.Eventf(set, v1.EventTypeWarning, "FailedDelete", "failed to delete pod %s: %v", pod.Name, err)
-		return false, err
+		return false, false, err
 	}
-	return true, nil
+	return true, true, nil
 }
 
 func (ssc *defaultStatefulSetControl) refreshPodState(set *appsv1beta1.StatefulSet, pod *v1.Pod, updateRevision string) (bool, time.Duration, error) {
