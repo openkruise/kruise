23080cd61c1fa4a84a4329e9bde9a9f96ce2ba8d
Peng Peng <ppbits3@gmail.com>
2025-02-26 23:53:36 -0800

    minor fix
    Signed-off-by: Peng Peng <ppbits3@gmail.com>

--- pkg/daemon/criruntime/factory_windows.go
+++ pkg/daemon/criruntime/factory_windows.go
@@ -20,8 +20,8 @@ limitations under the License.
 package criruntime
 
 import (
-	daemonutil "github.com/openkruise/kruise/pkg/daemon/util"
 	runtimeimage "github.com/openkruise/kruise/pkg/daemon/criruntime/imageruntime"
+	daemonutil "github.com/openkruise/kruise/pkg/daemon/util"
 )
 
 var (
