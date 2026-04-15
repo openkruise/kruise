1e0a6a2802eb67716dc53fc760b039769585768d
rakshaak29 <rakshaak29@gmail.com>
2026-04-15 21:55:12 +0530

    test: add unit tests for crr-workers flag validation and initialization
    Signed-off-by: rakshaak29 <rakshaak29@gmail.com>

---
+++ pkg/daemon/containerrecreate/crr_daemon_controller_test.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package containerrecreate
+
+import (
+	"testing"
+
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+func TestControllerRun(t *testing.T) {
+	c := &Controller{
+		workers: 5,
+		queue:   workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "crr-test"),
+	}
+
+	stop := make(chan struct{})
+	close(stop) // Close immediately so Run exits
+
+	// We can't easily test the informer sync because it's not set,
+	// but we can test that it doesn't crash and logs the workers count.
+	// We'll use a dummy informer that is already synced.
+	c.crrInformer = &fakeInformer{}
+
+	c.Run(stop)
+
+	if c.workers != 5 {
+		t.Errorf("expected workers 5, but got %d", c.workers)
+	}
+}
+
+type fakeInformer struct {
+	cache.SharedIndexInformer
+}
+
+func (f *fakeInformer) Run(stopCh <-chan struct{}) {}
+func (f *fakeInformer) HasSynced() bool           { return true }
---
+++ pkg/daemon/daemon_test.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2021 The Kruise Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package daemon
+
+import (
+	"testing"
+
+	"k8s.io/client-go/rest"
+)
+
+func TestNewDaemonValidation(t *testing.T) {
+	testCases := []struct {
+		name        string
+		cfg         *rest.Config
+		crrWorkers  int
+		expectError bool
+		errorMsg    string
+	}{
+		{
+			name:        "nil config",
+			cfg:         nil,
+			crrWorkers:  32,
+			expectError: true,
+			errorMsg:    "cfg can not be nil",
+		},
+		{
+			name:        "invalid crr workers (0)",
+			cfg:         &rest.Config{},
+			crrWorkers:  0,
+			expectError: true,
+			errorMsg:    "crr-workers must be greater than 0",
+		},
+		{
+			name:        "invalid crr workers (-1)",
+			cfg:         &rest.Config{},
+			crrWorkers:  -1,
+			expectError: true,
+			errorMsg:    "crr-workers must be greater than 0",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewDaemon(tc.cfg, "localhost:1234", 1, tc.crrWorkers)
+			if tc.expectError {
+				if err == nil {
+					t.Fatal("expected error, but got nil")
+				}
+				if err.Error() != tc.errorMsg {
+					t.Fatalf("expected error message %q, but got %q", tc.errorMsg, err.Error())
+				}
+			} else {
+				// We don't test success case deeply here as it would require mocking multiple clients
+				if err != nil {
+					t.Fatalf("expected no error, but got %v", err)
+				}
+			}
+		})
+	}
+}
